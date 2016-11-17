03539a4da4b6a013c5f37ad451f5a1d0a29e1a99
Форостяный Александр <bosmerer@gmail.com>
2016-11-17 17:36:52 +0200

    Working | Test

--- msp.php
+++ msp.php
@@ -243,7 +243,7 @@ class Multisite_Posts_Core {
 			foreach($pagination as $key => $item){
 				if(stripos($item, 'href')){
 					$item = str_replace("href", "hiddenHref", $item);
-					$page = substr($item, stripos($item,'paged')+1,strpos($item,'&',strpos($item,'paged')) - stripos($item,'paged')-1);
+					$page = substr($item, stripos($item,'page/')+5,strpos($item,'&',strpos($item,'page/')+5) - stripos($item,'page/')+4);
 					$pagination[$key] = substr_replace($item, " onclick='mspLoadPage($blog_id,$page);return false;'",strpos($item,'>'),0);
 				}
 			}
