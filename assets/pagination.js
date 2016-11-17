51122e81a22db5558f65a2c27df7e6ad39e4ff40
Форостяный Александр <bosmerer@gmail.com>
2016-11-17 13:26:04 +0200

    Working | Test

--- assets/pagination.js
+++ assets/pagination.js
@@ -2,9 +2,9 @@
  * Created by bosmer on 17.11.2016.
  */
 (function() {
-    function loadPageMSP(event) {
-        event.preventDefault();
+    function loadPageMSP() {
         console.log($(this).attr('hiddenHref'));
+        return false;
     }
     $(document).ready(function(){
         $(".msp-pagination a").on('click', 'loadPageMSP');
