cd345e137a29c3f0f64390d4011eac7cc0179307
Форостяный Александр <bosmerer@gmail.com>
2016-11-17 17:13:57 +0200

    Working | Test

--- assets/pagination.js
+++ assets/pagination.js
@@ -4,15 +4,16 @@
 $(document).ready(function(){
     console.log('pagination script loaded');
     $(".msp-pagination a").on('click', function(){
+        var blog_id = $(this).attr('blogid');
         var data = {
             'action': msppagination.action,
-            'blog_id': $(this).attr('blogid'),
+            'blog_id': blog_id,
             'pageNumber': $(this).text()
         };
         // We can also pass the url value separately from ajaxurl for front end AJAX implementations
         jQuery.post(msppagination.ajaxurl, data, function(response) {
-            var parent = $("ul[data-blogid = "+$(this).attr('blogid')+"]").parent();
-            $("ul[data-blogid = "+$(this).attr('blogid')+"]").remove();
+            var parent = $("ul[data-blogid = "+blog_id+"]").parent();
+            $("ul[data-blogid = "+blog_id+"]").remove();
             parent.append(response);
         });
         return false;
