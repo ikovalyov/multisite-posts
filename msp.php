195325a7b5469e4044a7406eecdadfcec269bcc2
Форостяный Александр <bosmerer@gmail.com>
2016-11-17 12:21:48 +0200

    Getting started

--- msp.php
+++ msp.php
@@ -178,7 +178,7 @@ class Multisite_Posts_Core {
 
 		$output  .= '</ul>';
 
-		$output .= msp_bootstrap_paginate_links($all_post->max_num_pages, $blog_id,$pageNumber);
+		$output .= msp_bootstrap_paginate_links(100, $blog_id, $pageNumber);
 
 		if($echo) {
 			echo $output;
