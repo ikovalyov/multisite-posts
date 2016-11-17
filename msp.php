426a23ea154690edecd3a73523ef366632e64c93
Форостяный Александр <bosmerer@gmail.com>
2016-11-17 16:53:31 +0200

    Working | Test

--- msp.php
+++ msp.php
@@ -241,7 +241,7 @@ class Multisite_Posts_Core {
 		if ( !empty( $pagination ) ) {
 			$pagination = array_map(function ($item) {
 				$item = str_replace("/m/page/", "/mpage/", $item);
-				$item = substr_replace($item, " blogid=asd",stripos($item,'\">')+1,0);
+				$item = substr_replace($item, " blogid='asd'",stripos($item,'>'),0);
 				return $item;
 			}, $pagination);
 		}
